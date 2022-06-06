f8d77b034150e0761ddf4a40c5494b7fb8c28062
Yanbo Liu <yliu431@sheffield.ac.uk>
2022-06-06 16:13:44 +0100

    fix

--- Data Structre and Algorithm/Complexity/different_function.py
+++ Data Structre and Algorithm/Complexity/different_function.py
@@ -2,12 +2,12 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-number=np.linspace(1,20,20)
+number=np.linspace(1,10,10)
 
 x1=number
 x2=np.power(number,2)
 x3=np.power(number,3)
-x4=np.ones(20)
+x4=np.ones(10)
 x5=np.multiply(np.log2(number),number)
 x6=np.power(2,number)
 
