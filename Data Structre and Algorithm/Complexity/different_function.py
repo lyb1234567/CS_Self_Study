5bbdf74288283a699019ce9650132dc3da32c085
Yanbo Liu <yliu431@sheffield.ac.uk>
2022-06-06 16:17:52 +0100

    fix

--- Data Structre and Algorithm/Complexity/different_function.py
+++ Data Structre and Algorithm/Complexity/different_function.py
@@ -9,13 +9,15 @@ x2=np.power(number,2)
 x3=np.power(number,3)
 x4=np.ones(10)
 x5=np.multiply(np.log2(number),number)
-x6=np.power(2,number)
+x6=np.log2(number)
+x7=np.power(2,number)
 
 
-plt.plot(number,x1,number,x2,number,x3,number,x4,number,x5,number,x6)
+
+plt.plot(number,x1,number,x2,number,x3,number,x4,number,x5,number,x6,number,x7)
 plt.xlabel("n")
 plt.ylabel("f(n)")
 plt.title("Comparison of increasing rate")
-plt.legend(["linear","power 2","power 3","constant","nlogn","expotential"])
+plt.legend(["linear","power 2","power 3","constant","nlogn","logn","a^n"])
 plt.savefig('image/Experiement.png')
 plt.show()
