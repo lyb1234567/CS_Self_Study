343a65009112e1f6f0f9470fc729db579da2e471
Yanbo Liu <yliu431@sheffield.ac.uk>
2022-07-11 11:23:43 +0100

    fix

--- C++ project/BookShop Management/BookShop Management/BookShop Management/BookShop Management.cpp
+++ C++ project/BookShop Management/BookShop Management/BookShop Management/BookShop Management.cpp
@@ -1,23 +1,60 @@
-#include "json/json.h"
+// Mysql_C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdlib.h>
 #include <iostream>
-#include <json/value.h>
-#include<json/json.h>
-#include<iostream>
-#include<string>
-#include<fstream>
-#include <conio.h>
-#include<cctype>
-#include<Windows.h>
-#include<fstream>
-#include "Used_function.h"
-#include <algorithm>
-#include <fstream>
-#include "Used_function.h"
-#include<iomanip>
-int main()
+#include <sstream>
+#include <stdexcept>
+/* uncomment for applications that use vectors */
+/*#include <vector>*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+#define EXAMPLE_HOST "localhost"
+#define EXAMPLE_USER "worlduser"
+#define EXAMPLE_PASS "worldpass"
+#define EXAMPLE_DB "world"
+
+using namespace std;
+
+int main(int argc, const char** argv)
 {
-    using namespace std;
-    Show_Menu();
-    system("explorer record.csv");
-    return 0;
+    string url(argc >= 2 ? argv[1] : EXAMPLE_HOST);
+    const string user(argc >= 3 ? argv[2] : EXAMPLE_USER);
+    const string pass(argc >= 4 ? argv[3] : EXAMPLE_PASS);
+    const string database(argc >= 5 ? argv[4] : EXAMPLE_DB);
+
+    cout << "Connector/C++ tutorial framework..." << endl;
+    cout << endl;
+
+    try {
+
+        /* INSERT TUTORIAL CODE HERE! */
+
+    }
+    catch (sql::SQLException& e) {
+        /*
+          MySQL Connector/C++ throws three different exceptions:
+
+          - sql::MethodNotImplementedException (derived from sql::SQLException)
+          - sql::InvalidArgumentException (derived from sql::SQLException)
+          - sql::SQLException (derived from std::runtime_error)
+        */
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+        /* what() (derived from std::runtime_error) fetches error message */
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+
+        return EXIT_FAILURE;
+    }
+
+    cout << "Done." << endl;
+    return EXIT_SUCCESS;
 }
